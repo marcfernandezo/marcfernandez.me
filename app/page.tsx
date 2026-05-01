f644dac51be5f39c7d01633c8553327b1f9636ce
Marc Fernandez <marc.fernaandezo@gmail.com>
2026-05-01 10:48:25 +0200

    feat: improve header description section

--- app/page.tsx
+++ app/page.tsx
@@ -34,7 +34,7 @@ export default function Home() {
     >
       <motion.section variants={VARIANTS_SECTION} transition={TRANSITION_SECTION}>
         <p className="text-zinc-600 dark:text-zinc-400">
-            Marc is a Barcelona-based Backend & Software Engineer who crafts systems 
+            Marc is a Barcelona-based Backend & Software Engineer who designs systems 
             where clean architecture meets purposeful engineering. He builds things 
             that don't just work, he builds things that matter.
         </p>
