34657bdf49980e7681061e1b223b258b987aa39b
Marc Fernandez <marc.fernaandezo@gmail.com>
2026-05-02 12:29:25 +0200

    feat: create blog page route

---
+++ app/blog/page.tsx
@@ -0,0 +1,10 @@
+export default function BlogPage() {
+  return (
+    <>
+      <h3 className="mb-5 text-lg font-mono font-semibold">Blog</h3>
+      <p className="text-neutral-500 dark:text-neutral-400">
+        This is where I share my thoughts and experiences on web development, programming, and technology. Stay tuned for updates!
+      </p>
+    </>
+  )
+}
